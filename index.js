2a114845ed4c6d59e5b4f4fcd691f09410dc0f4a
sanjidbillah <raiyansafir@gmail.com>
2022-06-19 14:45:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const PORT = 3002;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const app = express();
+
+async function latestPriceScrapping() {
+  const url = "http://dam.gov.bd/?L=B";
+  const priceArray = [];
+  await axios(url).then((response) => {
+    const html_data = response.data;
+    const $ = cheerio.load(html_data);
+    $('#wrapper > div > #bcontainer > #bbody > div.mymarquee > #marqueeborder > #marqueecontent > span.stockbox ').each((index, elem) => {
+
+      $(elem).children().each((childID, childElem) => {
+
+        priceArray.push($(childElem).parent().next().text());
+      })
+    })
+
+
+  });
+  return priceArray;
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const pricingLIst = await latestPriceScrapping();
+    return res.status(200).json(pricingLIst);
+  } catch (err) {
+    return res.status(500).json({
+      err: err.toString(),
+    });
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`The server is active and running on port ${PORT}`)
+);
+
